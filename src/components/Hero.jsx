e4fd4ec380f56945acdd7056c2b900c887cfdbb8
PhuocMinh111 <96472489+PhuocMinh111@users.noreply.github.com>
2022-06-24 16:10:35 +0700

    commit 2

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+export default class Hero extends React.Component {
+  render() {
+    return (
+      <Container>
+        <img src="./data/" alt="" />
+      </Container>
+    );
+  }
+}
