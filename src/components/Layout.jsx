54b9619393485d0103831d3e3f7826d81203531d
PhuocMinh111 <96472489+PhuocMinh111@users.noreply.github.com>
2022-06-27 11:42:55 +0700

    fix hero

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -18,7 +18,7 @@ export default class Layout extends React.Component {
   render() {
     return (
       <>
-        <div className="container-fluid mb-3">
+        <div className="container mb-3">
           <div className="row">
             <div className="col-lg-6 p-5">
               <Model
